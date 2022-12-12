b8230f0916214a98e22cf32615fc7523fa226964
AndrelDaga <andreldaga21@gmail.com>
2022-12-12 14:35:29 -0800

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Description: 'The program's description'
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		if (i == 57)
+		{
+			continue;
+		}
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+
+	return (0);
+}
