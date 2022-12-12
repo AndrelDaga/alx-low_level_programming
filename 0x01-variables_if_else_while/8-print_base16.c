86558a4a954fba2c3f40e9ed017cc0e4b51129bd
AndrelDaga <andreldaga21@gmail.com>
2022-12-12 14:20:56 -0800

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Description: 'The program's description'
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	int j;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (j = 97; j < 103; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+
+	return (0);
+}
