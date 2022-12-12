c88139e951eee95e2f9bf119e6e5b265c6cad9b7
AndrelDaga <andreldaga21@gmail.com>
2022-12-12 14:12:12 -0800

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Description: 'the program's description'
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 122; i >= 97; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
+
