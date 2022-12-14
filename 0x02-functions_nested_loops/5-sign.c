b4cadd012e5aa7939d9635b90d174fa7977c1651
AndrelDaga <andreldaga21@gmail.com>
2022-12-14 10:22:01 -0800

    Task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * print_sign - prints of a number
+  * @n: number to be checked
+  * Return: returns certain number accordingly
+  */
+
+int print_sign(int n)
+{
+	if (n > 0)
+		_putchar('+');
+	else if ( n == 0)
+		_putchar('0');
+		return (0);
+	else
+		_putchar('-');
+		return (-1);
+}
