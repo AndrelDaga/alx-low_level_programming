baebb488950dce54b5df561b06455937646976c3
AndrelDaga <andreldaga21@gmail.com>
2022-12-15 14:14:51 -0800

    Task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+  * fizz_buzz - prints number, fizz, buzz based on some conditions
+  *Return: it returns nothing just prints out the specified
+  */
+
+void fizz_buzz(void)
+{
+	int i;
+
+	i = 1;
+	while (i < 101)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+			i = i + 1;
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+			i = i + 1;
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+			i = i + 1;
+		}
+		else
+		{
+			printf("%d ", i);
+			i = i + 1;
+		}
+	}
+}
