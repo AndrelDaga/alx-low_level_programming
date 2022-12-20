463f5d667f61cad2be18fdc1b2a359732ae0114a
AndrelDaga <andreldaga21@gmail.com>
2022-12-19 22:07:56 -0800

    Cpmpleted task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include <string.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the string
+ * Return: returns and integer value
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
