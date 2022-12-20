3f72ca99d7738d4cc93829a129d6121dc2659e87
AndrelDaga <andreldaga21@gmail.com>
2022-12-20 09:55:41 -0800

    complete task7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -22,7 +22,7 @@ void puts_half(char *str)
 	f = (l - 1) / 2;
 	if (l % 2 != 0)
 	{
-		for (i = f; i <= l-1; i++)
+		for (i = f; i <= l - 2; i++)
 		{
 			_putchar(str[i]);
 		}
@@ -30,7 +30,7 @@ void puts_half(char *str)
 	}
 	else
 	{
-		for (i = t; i <= l-1; i++)
+		for (i = t; i <= l - 1; i++)
 		{
 			_putchar(str[i]);
 		}
