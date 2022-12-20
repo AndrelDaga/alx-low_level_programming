b84dfa17ee175eac2639bcb5143b068b005ff585
AndrelDaga <andreldaga21@gmail.com>
2022-12-20 10:25:02 -0800

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -18,7 +18,10 @@ void print_array(int *a, int n)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
-		printf(", ");
+		if ( i == n)
+			continue;
+		else
+			printf(", ");
 	}
 	printf("\n");
 }
