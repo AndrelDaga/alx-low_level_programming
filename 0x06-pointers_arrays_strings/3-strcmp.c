5fa450fc82fc79a2e7bc02807f5f71c8fc3e468b
AndrelDaga <andreldaga21@gmail.com>
2022-12-22 14:57:31 -0800

    Completed task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include <string.h>
+
+/**
+ * _strcmp - compares two strings
+ *
+ *Description: 'Program's description'
+ *
+ *@s1: string 1
+ *@s2: string 2
+ *
+ *Return: and integer
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
