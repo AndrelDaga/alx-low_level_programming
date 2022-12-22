b9afa7cb551bb594602fe362c5163522e32b86d7
AndrelDaga <andreldaga21@gmail.com>
2022-12-22 14:23:12 -0800

    Completed task 6

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,17 @@
+#include <string.h>
+
+/**
+ * _strcat - concatenates two strings
+ *
+ *Description: 'Program's description'
+ *
+ *@dest: destination
+ *@src: source
+ *
+ *Return: return a pointer to the string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
