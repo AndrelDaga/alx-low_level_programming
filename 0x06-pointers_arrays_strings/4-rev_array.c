32ed49a8a36c4986190dee8e347d309aca553afc
AndrelDaga <andreldaga21@gmail.com>
2022-12-22 15:39:32 -0800

    task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -18,7 +18,10 @@ void reverse_array(int *a, int n)
 
 	for (i = n - 1; i >= 0; i--)
 	{
-		printf("%d ", a[i]);
+		printf("%d", a[i]);
+		if (i == 0)
+			continue;
+		printf(", ");
 	}
 	printf("\n");
 }
