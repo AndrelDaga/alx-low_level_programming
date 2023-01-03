12b74362c8522a00e6babb1914dec612a14b54b7
AndrelDaga <andreldaga21@gmail.com>
2023-01-03 12:48:15 -0800

    Task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+#include <string.h>
+
+/**
+ *_strchr - locates a character in a string
+ *
+ *@s: param1
+ *@c: param2
+ *
+ *Description: 'Program's description'
+ *
+ *Return: a pointer
+ */
+
+char *_strchr(char *s, char c)
+{
+	return (strchr(s, c));
+}
