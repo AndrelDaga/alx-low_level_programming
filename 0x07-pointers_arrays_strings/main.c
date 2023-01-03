b8940f4c6e05cfcb1ab1dd7589318c440c45830b
AndrelDaga <andreldaga21@gmail.com>
2023-01-03 12:22:43 -0800

    Completed task 0

---
+++ 0x07-pointers_arrays_strings/main.c
@@ -0,0 +1,14 @@
+#include <string.h>
+
+/**
+ * _memset - fills memory with a constant byte.
+ *
+ *Description: 'Program's description'
+ *
+ *Return: a pointer
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	return (memset(s, b, n));
+}
