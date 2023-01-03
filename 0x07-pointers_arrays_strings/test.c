989091187808157e0a5a2390017d6b82fde2560c
AndrelDaga <andreldaga21@gmail.com>
2023-01-03 13:29:29 -0800

    Task 6

--- 0x07-pointers_arrays_strings/test.c
+++ 0x07-pointers_arrays_strings/test.c
@@ -1,35 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 
-void simple_print_buffer(char *buffer, unsigned int size)
-{
-	unsigned int i;
-
-	i = 0;
-	while (i < size)
-	{
-		if (i % 10)
-		{
-			printf(" ");
-		}
-		if (!(i % 10) && i)
-		{
-			printf("\n");
-		}
-		printf("0x%02x", buffer[i]);
-		i++;
-	}
-	printf("\n");
-}
-
 int main(void)
 {
-	char buffer[98] = {0x00};
-
-	simple_print_buffer(buffer, 98);
-	_memset(buffer, 0x01, 95);
-	printf("__________________________________\n");
-	simple_print_buffer(buffer, 98);
+	char board[8][8] = {
+		{'r', 'k', 'b', 'q', 'k', 'b', 'k', 'r'},
+		{'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+	};
+	print_chessboard(board);
 	return (0);
 }
-
