42f30e4ea1e331ed230128ac5cefe92fe504131b
AndrelDaga <andreldaga21@gmail.com>
2023-01-03 13:07:40 -0800

    Task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,17 @@
+#include <string.h>
+
+/**
+ * _strstr - locates a substring
+ *
+ *@haystack: param1
+ *@needle: param2
+ *
+ *Description: 'Program's description'
+ *
+ *Return: a pointer
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	return (strstr(haystack, needle));
+}
