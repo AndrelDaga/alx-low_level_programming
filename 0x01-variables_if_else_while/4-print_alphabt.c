7ea93b4a50cb7343f444f16c2c4dd82b6ee9456f
AndrelDaga <andreldaga21@gmail.com>
2023-05-19 18:14:55 +0200

    Updated task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints out a sentence
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	a = 97;
+	while (a < 123)
+	{
+		if (a == 101 || a == 113)
+		{
+			a++;
+			continue;
+		}
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
