14a4e3368ecec2b9a738dc93b432804c4e702e73
AndrelDaga <andreldaga21@gmail.com>
2023-05-19 14:42:15 +0200

    Completed task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints out a sentence
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	int b;
+
+	a = 97;
+    b = 65;
+	while (a < 123)
+	{
+		putchar(a);
+		a++;
+	}
+    while (b < 91)
+    {
+        putchar(b);
+        b++;
+    }
+	putchar('\n');
+	return (0);
+}
