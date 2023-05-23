1f31502e1a8a874616ad03ce92fd377b0accafb0
AndrelDaga <andreldaga21@gmail.com>
2023-05-23 13:06:15 +0200

    updated task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,7 +3,8 @@
 
 /**
  * _abs - computes the absolute value of an integer
- * @int: the integer value to work with
+ * Description: as described above
+ * @n: the integer value to work with
  * Return: the positive integer value
  */
 int _abs(int n)
