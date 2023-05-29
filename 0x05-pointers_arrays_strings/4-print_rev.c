54fb7f217700b965ebe811c2fa828d142d634427
AndrelDaga <andreldaga21@gmail.com>
2023-05-29 18:10:45 +0200

    completed task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ * @s: the string
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	int x;
+
+	x = strlen(s);
+	while (x >= 0)
+	{
+		_putchar(s[x]);
+		x--;
+	}
+	_putchar('\n');
+}
