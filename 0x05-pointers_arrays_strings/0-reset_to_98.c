77d04f9d3b450bc7adc550971eed0a3949f7829d
AndrelDaga <andreldaga21@gmail.com>
2023-05-29 17:26:37 +0200

    Completed task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes parameter and updates the value
+ *  it points to to 98
+ *  @n: the parameter of the function
+ *  Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
