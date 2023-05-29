e7fd18d25ab4e6b5e2d426564daf24f5de36c381
AndrelDaga <andreldaga21@gmail.com>
2023-05-30 00:45:53 +0200

    completed task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include <string.h>
+
+/**
+ *_strcpy - copies the string pointer to by a source
+ *
+ *Description: 'Program's description'
+ *
+ *@dest: destination
+ *@src: source
+ *
+ *Return: it returns the pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(dest, src));
+}
