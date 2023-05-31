903871e195c4bc5a1e81e3726d76831e91b65049
AndrelDaga <andreldaga21@gmail.com>
2023-05-31 21:45:03 +0200

    Completed task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: the string
+ * Return: a pointer
+ */
+void *cap_string(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == ',' || s[i] == ';' || s[i] == '.')
+		{
+			s[i + 1] = toupper(s[i + 1]);
+		}
+		if (s[i] == '!' || s[i] == '?' || s[i] == '""')
+		{
+			s[i + 1] = toupper(s[i + 1]);
+		}
+		if (s[i] == '(' || s[i] == ')' || s[i] == '{')
+		{
+			s[i + 1] = toupper(s[i + 1]);
+		}
+		if (s[i] == '}' || s[i] == '\n', s[i] == '\t')
+		{
+			s[i + 1] = toupper(s[i + 1]);
+		}
+		i++;
+	}
+	return (s);
+}
