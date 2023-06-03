0cb1009474ba4c8b152e1103e3d7e18e44e685c7
AndrelDaga <andreldaga21@gmail.com>
2023-06-03 15:31:12 +0200

    updated task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,15 +8,26 @@
  */
 void reverse_array(int *a, int n)
 {
-	int x;
+	int temp;
 
 	int i;
 
-	for (i = 0; i < (n / 2) + 2; i++)
+	int loop_count;
+
+	if ((n % 2) == 0)
 	{
-		x = a[i];
+		loop_count = n / 2;
+	}
+	else
+	{
+		loop_count = (n / 2) + 1;
+	}
+
+	for (i = 0; i < loop_count; i++)
+	{
+		temp = a[i];
 		a[i] = a[n - 1];
-		a[n - 1] = x;
+		a[n - 1] = temp;
 		n--;
 	}
 }
