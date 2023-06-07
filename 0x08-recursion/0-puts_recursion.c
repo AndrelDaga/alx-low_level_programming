e535637f31433557aae13f92e08d29eca9d2bb99
AndrelDaga <andreldaga21@gmail.com>
2023-06-07 12:05:13 +0200

    Completed task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - prints a string follwed by a new line.
+ * @s: the string
+ * Return: nothing
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
