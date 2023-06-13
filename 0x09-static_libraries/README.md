7b1730d4c932c5f8e433347515c00c3e7e8bd92c
AndrelDaga <andreldaga21@gmail.com>
2023-06-13 12:34:24 +0200

    Completed task 1

--- 0x09-static_libraries/README.md
+++ 0x09-static_libraries/README.md
@@ -1,1 +1,2 @@
 This directory contains answers to tasks on creating static libraries in C
+the create_static_lib.sh file creates a static library call liball
--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c *.c | ar -rc liball.a *.o
+
+echo "the create_static_lib.sh file creates a static library call liball" >> README.md
