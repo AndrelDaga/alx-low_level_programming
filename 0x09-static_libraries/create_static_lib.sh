92d4d7a31eb04fb0b0856f77997ffddc5e0360b7
AndrelDaga <andreldaga21@gmail.com>
2023-06-13 13:34:25 +0200

    Updated task 1

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c *.c | ar -rc liball.a *.o > ranlib
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c *.c | ar -rc liball.a *.o | ranlib liball.a
 
 echo "the create_static_lib.sh file creates a static library call liball" >> README.md
