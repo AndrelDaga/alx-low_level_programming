99cddf8d10080cc57bc9da90266002d462ce4d21
AndrelDaga <andreldaga21@gmail.com>
2023-06-19 13:01:11 +0200

    updated task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog 
+/**
+ * struct dog - describes a dog
+ * @name: the name of a dog
+ * @age: the age of the dog
+ * @owner: the dog's owner
+ */
+struct dog
 {
 	char *name;
 	float age;
