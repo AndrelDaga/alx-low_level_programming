db8bbed2fa3645bf6b4435c368855a304dcf448a
AndrelDaga <andreldaga21@gmail.com>
2023-06-29 22:10:02 +0200

    Fixed betty on lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stddef.h>
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
 typedef struct list_s
 {
 	char *str;
