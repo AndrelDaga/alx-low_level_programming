2f79f8375c85dd9ad01ab96d488e54586d037321
AndrelDaga <andreldaga21@gmail.com>
2023-06-30 00:23:00 +0200

    Completed task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_list - frees a list_t list
+ * @head: the head of the list
+ * Return: nothing
+ */
+void free_list(list_t *head)
+{
+	if(head == NULL)
+	{
+		return;
+	}
+	else
+	{
+		if (head->next != NULL)
+		{
+			free_list(head->next);
+		}
+		free(head);
+	}
+}
