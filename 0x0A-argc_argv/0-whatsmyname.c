f8c8e4ae719a42bc2a3959e11d185698e51f3b92
AndrelDaga <andreldaga21@gmail.com>
2023-07-02 09:05:28 +0200

    fixed betty

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * @argv: the command line arguments
  * Return: an integer 0;
  */
-int main(int argc __attribute__((unused)) , char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
