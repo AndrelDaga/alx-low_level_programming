bff4927fd2eb21f549f9a94c969c82c0053622e2
AndrelDaga <andreldaga21@gmail.com>
2023-07-06 13:39:13 -0700

    completed task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_binary - prints the binary rep of a number
+ * @n: the number
+ * Return: nothing
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int size, mask, l;
+	int i;
+
+	size = sizeof(int) * 8;
+	l = 0;
+
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+	for (i = size - 1; i >= 0; i--)
+	{
+		mask = n & (1 << i);
+		if (mask == 0)
+		{
+			if (l)
+			{
+				_putchar('0');
+			}
+		}
+		else
+		{
+			_putchar('1');
+			l = 1;
+		}
+	}
+	_putchar('\n');
+}
+
