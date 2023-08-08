481180fd41445b7b4da5688280ff91073bc1bc10
root <root@ROOT>
2023-08-08 23:40:07 +0200

    refactored

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -25,7 +25,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	buf = malloc(sizeof(char) * letters + 1);
+	buf = malloc(sizeof(char) * (letters + 1));
 	if (buf == NULL)
 	{
 		close(fd);
